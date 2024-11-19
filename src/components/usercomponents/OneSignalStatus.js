317ef498df0bebbb5fac048403c6390cf3889c88
dsatoh20 <dsatoh@GQ2DPW2>
2024-11-19 17:04:11 +0900

    added showSlidedownPrompt

--- src/components/usercomponents/OneSignalStatus.js
+++ src/components/usercomponents/OneSignalStatus.js
@@ -1,12 +1,13 @@
 const handleCheckStatus = () => {
     if (window.OneSignal) {
-        window.OneSignal.push(function() {
+        window.OneSignal.push(() => {
             // 通知が有効か確認
             window.OneSignal.isPushNotificationsEnabled(function(isEnabled) {
                 if (isEnabled) {
                     alert("通知は有効になっています！");
                 } else {
-                    alert("通知は無効です。");
+                    alert("通知は無効です。ぜひ有効化してください。リマインダーを送りますよ。");
+                    window.OneSignal.showSlidedownPrompt();
                 }
             });
         });
