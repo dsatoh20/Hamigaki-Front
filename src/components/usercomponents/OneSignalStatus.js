67dd1f118534abab559a2716757c6c833bec1ca2
dsatoh20 <dsatoh@GQ2DPW2>
2024-11-19 21:50:11 +0900

    push notif for ios

--- src/components/usercomponents/OneSignalStatus.js
+++ src/components/usercomponents/OneSignalStatus.js
@@ -1,13 +1,15 @@
+var OneSignal = window.OneSignal || [];
+
 const handleCheckStatus = () => {
-    if (window.OneSignal) {
-        window.OneSignal.push(() => {
+    if (OneSignal) {
+        OneSignal.push(function() {
             // 通知が有効か確認
-            window.OneSignal.isPushNotificationsEnabled(function(isEnabled) {
+            OneSignal.isPushNotificationsEnabled(function(isEnabled) {
                 if (isEnabled) {
                     alert("通知は有効になっています！");
                 } else {
                     alert("通知は無効です。ぜひ有効化してください。リマインダーを送りますよ。");
-                    window.OneSignal.showSlidedownPrompt();
+                    OneSignal.showSlidedownPrompt();
                 }
             });
         });
